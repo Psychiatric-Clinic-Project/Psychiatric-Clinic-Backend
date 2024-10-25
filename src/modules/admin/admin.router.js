dead32b5ef303b325ec320fda1727b6724ada13b
FatimaOmair <fatimaemer2002@gmail.com>
2024-10-25 21:52:34 +0300

    SEA-18: create article model, add article, delete an article ,get articles , get an article by id

--- src/modules/admin/admin.router.js
+++ src/modules/admin/admin.router.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { HME, multerValidation, myMulter } from "../../services/multer.js";
-import { adminSignUp } from "./admin.controller.js";
+import { addArticle, adminSignUp, deleteArticle, getArticle, getArticleById } from "./admin.controller.js";
 
 const router = Router();
 
 router.post("/signup",myMulter(multerValidation.image).single("image"),HME,adminSignUp)
+router.post("/add/article",myMulter(multerValidation.image).single("image"),HME,addArticle)
+router.get("/allArticles",getArticle);
+router.delete("/deleteArticle/:id",deleteArticle)
+router.get("/article/:id", getArticleById);
+
+
+
 
 export default router;
