b1dd0c8edf4827a8a10a1cd897a87586a2aa79da
Osama Maree <osamamarei2001@gmail.com>
2024-10-26 17:34:02 +0200

    SEA-30: Import with s

--- src/modules/admin/admin.router.js
+++ src/modules/admin/admin.router.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { HME, multerValidation, myMulter } from "../../services/multer.js";
-import { addArticle, adminSignUp, deleteArticle, getArticle, getArticleById } from "./admin.controller.js";
+import { addArticle, adminSignUp, deleteArticle, getArticleById, getArticles } from "./admin.controller.js";
 import { asyncHandler } from "../../middleWare/asyncHandler.js";
 
 const router = Router();
 
 router.post("/signup",myMulter(multerValidation.image).single("image"),HME,asyncHandler(adminSignUp))
 router.post("/add/article",myMulter(multerValidation.image).single("image"),HME,asyncHandler(addArticle))
-router.get("/allArticles",getArticle);
+router.get("/allArticles",asyncHandler(getArticles));
 router.delete("/deleteArticle/:id",asyncHandler(deleteArticle))
 router.get("/article/:id", asyncHandler(getArticleById));
 
