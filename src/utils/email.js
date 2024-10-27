5a027d6edb194bca75942c84c98a9c1ed21bd8c3
FatimaOmair <fatimaemer2002@gmail.com>
2024-10-27 19:55:59 +0200

    SEA-21: remove comments and spaces

--- src/utils/email.js
+++ src/utils/email.js
@@ -5,33 +5,24 @@ const sendEmail = async (username, to, verificationUrl) => {
     if (!to) {
         throw new Error("No recipient email address provided");
     }
-
     let transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
             user: process.env.EMAIL, 
-            pass: process.env.PASSWORD, // Use your app password here
+            pass: process.env.PASSWORD, 
         },
         tls: {
             rejectUnauthorized: false,
         },
     });
-
-    const emailContent = emialTemplate(username, verificationUrl); // Adjust according to your template function
-
-    try {
+    const emailContent = emialTemplate(username, verificationUrl); 
         let info = await transporter.sendMail({
             from: process.env.EMAIL, // Sender address
             to, // Recipient's email
             subject: "Please verify your email",
             html: emailContent,
         });
-
-        console.log("Email sent successfully: ", info.messageId);
-    } catch (error) {
-        console.error("Error sending email: ", error);
         throw new Error("Could not send email");
-    }
 };
 
 export default sendEmail;
