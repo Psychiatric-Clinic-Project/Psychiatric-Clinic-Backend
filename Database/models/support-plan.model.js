2230002865d900e1c55c6983298d9f07f30fbda7
Osama Maree <osamamarei2001@gmail.com>
2024-11-02 16:51:01 +0200

    SEA-32: format schema

--- Database/models/support-plan.model.js
+++ Database/models/support-plan.model.js
@@ -1,31 +1,34 @@
-import { Schema, model, Types } from 'mongoose';
-import { ROLES } from '../../src/constant.js';
+import { Schema, model, Types } from "mongoose";
+import { ROLES } from "../../src/constant.js";
 
-const supportPlanSchema = new Schema({
-  advisorId: {
-    type: Types.ObjectId,
-    ref: "Advisor",
+const supportPlanSchema = new Schema(
+  {
+    advisorId: {
+      type: Types.ObjectId,
+      ref: "Advisor",
+    },
+    coachId: {
+      type: Types.ObjectId,
+      ref: "Coach",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    createdByRole: {
+      type: String,
+      enum: [ROLES.advisor, ROLES.coach],
+      required: true,
+    },
   },
-  coachId: {
-    type: Types.ObjectId,
-    ref: "Coach",
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  content: {
-    type: String,
-    required: true
-  },
-  createdByRole: {
-    type: String,
-    enum: [ROLES.advisor,ROLES.coach],
-    required: true
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
 const SupportPlan = model("SupportPlan", supportPlanSchema);
 
