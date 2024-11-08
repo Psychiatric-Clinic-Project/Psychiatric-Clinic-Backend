bb5f242a9cf4158cc329c846449c9c12cf96aeb3
FatimaOmair <fatimaemer2002@gmail.com>
2024-11-08 16:07:18 +0200

    chat-feature: Complete endpoint for Chat

--- src/modules/chat/chat.controller.js
+++ src/modules/chat/chat.controller.js
@@ -3,6 +3,7 @@ import messageModel from "../../../Database/models/message.model.js";
 import participantModel from "../../../Database/models/participant .model.js";
 import {
   createdSuccessfullyMessage,
+  deletedSuccessfullyMessage,
   mapToOriginId,
   notFoundMessage,
   retrievedSuccessfullyMessage,
@@ -12,7 +13,7 @@ export const createNewChat = async (req, res) => {
   const { isGroup, chatName, participantIds } = req.body;
   let chat;
   if (isGroup) {
-    adminId = await participantModel
+   const adminId = await participantModel
       .find(mapToOriginId(req.user.role, req.user._id))
       .select("_id");
     if (!adminId) {
@@ -20,7 +21,7 @@ export const createNewChat = async (req, res) => {
     }
     chat = await chatModel.create({
       participantIds,
-      groupAdmin: adminId,
+      groupAdmin: adminId[0]._id,
       isGroup,
       chatName,
     });
@@ -46,7 +47,7 @@ export const sendMessage = async (req, res) => {
   return res.success(message, createdSuccessfullyMessage("Message"), 201);
 };
 
-export const retrieveChats = async () => {
+export const retrieveChats = async (req,res) => {
   const participantId = await participantModel
     .find(mapToOriginId(req.user.role, req.user._id))
     .select("_id");
@@ -60,7 +61,7 @@ export const retrieveChats = async () => {
   return res.success(chats, retrievedSuccessfullyMessage("Chats"), 200);
 };
 
-export const retriveMessagesForChat = async () => {
+export const retriveMessagesForChat = async (req,res) => {
   const { chatId } = req.params;
   const messages = await messageModel
     .find({
@@ -70,3 +71,50 @@ export const retriveMessagesForChat = async () => {
 
   return res.success(messages, retrievedSuccessfullyMessage("Messages"), 200);
 };
+
+export const leaveChat = async (req, res) => {
+    const { chatId } = req.params;
+    const participantId = await participantModel
+     .find(mapToOriginId(req.user.role, req.user._id))
+     .select("_id");
+    const chat = await chatModel.findByIdAndUpdate(
+      chatId,
+      { $pull: { participantIds: participantId[0]._id } },
+      { new: true }
+    );
+    if (!chat) {
+      return res.error(notFoundMessage("Chat"), 404);
+    }
+    return res.success(chat, "chat left successfully", 200);
+}
+
+export const deleteChat = async (req, res) => {
+    const { chatId } = req.params;
+    const chat = await chatModel.findByIdAndDelete(chatId);
+    if (!chat) {
+      return res.error(notFoundMessage("Chat"), 404);
+    }
+    return res.success(chat, deletedSuccessfullyMessage("Chat"), 200);
+}
+
+export const deleteMessage = async (req, res) => {
+    const { messageId } = req.params;
+    const message = await messageModel.findByIdAndDelete(messageId);
+    if (!message) {
+      return res.error(notFoundMessage("Message"), 404);
+    }
+    return res.success(message, deletedSuccessfullyMessage("Message"), 200);
+}
+
+export const createMessage = async(req,res)=>{
+    const { chatId, content } = req.body;
+    const participantId = await participantModel
+     .find(mapToOriginId(req.user.role, req.user._id))
+     .select("_id");
+    const message = await messageModel.create({
+      chatId,
+      sender: participantId[0]._id,
+      content,
+    });
+    return res.success(message, createdSuccessfullyMessage("Message"), 201);
+}
--- src/modules/chat/chat.router.js
+++ src/modules/chat/chat.router.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { asyncHandler } from "../../middleWare/asyncHandler.js";
-import { createNewChat, retrieveChats, retriveMessagesForChat } from "./chat.controller.js";
+import { createMessage, createNewChat, deleteChat, deleteMessage, leaveChat, retrieveChats, retriveMessagesForChat } from "./chat.controller.js";
 import { ROLES } from "../../constant.js";
 import { auth } from "../../middleWare/auth.js";
 
@@ -10,5 +10,9 @@ router.use(auth([ROLES.advisor,ROLES.user,ROLES.coach]));
 router.post('/create-chat',asyncHandler(createNewChat))
 router.get('/retrive-chats',asyncHandler(retrieveChats))
 router.get('/retrive-messages',asyncHandler(retriveMessagesForChat))
+router.patch('/leave-chat/:chatId',asyncHandler(leaveChat))
+router.delete('/delete-chat/:chatId',asyncHandler(deleteChat))
+router.delete('/delete-message/:messageId',asyncHandler(deleteMessage))
+router.post('/create-message',asyncHandler(createMessage))
 
 export default router;
